ff4ba60285f7a636b7b24d6eabfde1d55e85f836
yousseffkhaledd <youssefkmamdouh@gmail.com>
2025-11-14 18:45:09 +0200

    Remove unnecessary modules folder

--- src/modules/organization-structure/departments/departments.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DepartmentsService } from './departments.service';
-import { DepartmentsController } from './departments.controller';
-
-@Module({
-  providers: [DepartmentsService],
-  controllers: [DepartmentsController]
-})
-export class DepartmentsModule {}
